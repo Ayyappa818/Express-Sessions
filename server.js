425fccc6049333d1560a8d7f69f4d5d393a39650
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-11-26 19:36:18 +0530

    changes

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ var app = express();
 var fs = require("fs");
 var cors = require('cors')
 app.use(cors())
+
+var bodyParser = require("body-parser")
+// app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 app.get('/products',(req,res)=>{
     var product = JSON.parse(fs.readFileSync("Products.txt").toString());
     res.json(product)
     console.log(product)
 })
+
 app.get('/products/:id',(req,res)=>{
     var pid = JSON.parse(fs.readFileSync("Products.txt").toString());
     pid.map((p,i)=>{
@@ -17,6 +23,22 @@ app.get('/products/:id',(req,res)=>{
         }
     })
 })
+
+app.post('/addproduct',(req,res)=>{
+    var product = JSON.parse(fs.readFileSync("Products.txt").toString());
+    product.push(req.body)
+    res.json(product)
+    fs.writeFileSync('Products.txt',JSON.stringify(product))
+    res.send("data was added")
+})
+
+app.delete('deleteproduct/:id',(req,res)=>{
+    var del = JSON.parse(fs.readFileSync("Products.txt").toString());
+    del.splice(req.params.id,1)
+    fs.writeFileSync('Products.txt',JSON.stringify(del))
+    res.send("product deleted")
+})
+
 app.listen(8500,()=>{
     console.log("server is running on port 8500")
 })
